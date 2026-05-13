14d34bd78de32c48b5070e0c0ff1f50eae663eb1
DruideCalme <benjamin.chassier@gmail.com>
2026-05-13 12:49:34 +0200

    Troisieme commit avec AuthContext

--- ProjetFinal/src/components/Navbar.jsx
+++ ProjetFinal/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 
 const Navbar = () => {
     const navigate = useNavigate();
 
-    const user = false; // Simuler un utilisateur connecté ou non
+    const { user, logout } = useAuth(); 
+    
 
     const handleLogout = () => {
     console.log("Déconnexion en cours...");
+    logout();
     navigate('/login');
     };
 
